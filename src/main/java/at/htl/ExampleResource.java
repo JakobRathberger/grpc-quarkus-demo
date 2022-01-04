0181b0ae870b7d429425ea3a9bea22b333d627f4
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-01-04 02:32:35 +0100

    init

--- src/main/java/at/htl/ExampleResource.java
+++ src/main/java/at/htl/ExampleResource.java
@@ -1,16 +1,32 @@
 package at.htl;
 
+import io.quarkus.example.Greeter;
+import io.quarkus.example.HelloRequest;
+import io.quarkus.grpc.GrpcClient;
+import io.smallrye.mutiny.Uni;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class ExampleResource {
 
+    @GrpcClient
+    Greeter hello;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy Reactive";
+        return "hello";
+    }
+
+    @GET
+    @Path("/{name}")
+    public Uni<String> hello(@PathParam("name") String name) {
+        return hello.sayHello(HelloRequest.newBuilder().setName(name).build())
+                .onItem().transform(helloReply -> helloReply.getMessage());
     }
 }
